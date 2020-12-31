d0f9dddbf9ff8c373b585668a6d3fda73c94bd37
Hassanbhatti <66300348+HassanrazaBhatti@users.noreply.github.com>
2021-01-01 00:15:29 +0500

    Create app.js

---
+++ Programe 4/app.js
@@ -0,0 +1,22 @@
+
+// Question No 4
+
+//program to check if a number is prime or not
+ const number = parseInt(prompt("Enter a positive number: "));
+ let isPrime = true;
+ if (number === 1) {
+     console.log("1 is not a prime number.");
+ }
+ else if (number > 1) {
+     for (let i = 2; i < number; i++) {
+         if (number % i == 0) {
+             isPrime = false;
+             break;
+         }
+     }
+     if (isPrime) {
+         console.log(`${number} is a prime number`);
+     } else {
+         console.log(`${number} is a not prime number`);
+     }
+ }
